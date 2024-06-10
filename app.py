dbae98d97528f717fb817da97e1e3c8a744d8b51
Firework-133 <2416009229@qq.com>
2024-06-10 18:03:22 +0800

    android兼容测试

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import os, sys
+import sys
+from pathlib import Path
 from io import BytesIO
 from flask import Flask, render_template, send_file
 import webview
@@ -9,7 +10,8 @@ height = 800
 min_width = 790
 min_height = 630
 
-music_directory: list = [f"{os.path.expanduser('~')}\\Music"]
+window = None
+music_directory: list = []
 audio_files_info_dict = {}
 app = Flask(
     __name__,
@@ -65,23 +67,19 @@ def get_audio_file_dict(directory_paths: list):
         dict: 一个字典，其中键是文件名，值是绝对文件路径。
     """
     audio_files_dict = {}  # 创建一个空字典来存储文件名和绝对路径
+    audio_extensions = (".mp3", ".wav", ".flac", ".ogg", ".m4a", ".aac", ".wma", ".ape")
 
     for directory_path in directory_paths:
-        for root, dirs, files in os.walk(directory_path):
-            for filename in files:
-                if filename.lower().endswith(
-                    (".mp3", ".wav", ".flac", ".ogg", ".m4a", ".aac", ".wma", "ape")
-                ):
-                    file_abs_path = os.path.join(root, filename)  # 获取文件的绝对路径
-                    audio_files_dict[filename] = (
-                        file_abs_path  # 将文件名和绝对路径添加到字典中
-                    )
+        path = Path(directory_path)
+        for file_path in path.rglob("*"):
+            if file_path.suffix.lower() in audio_extensions:
+                audio_files_dict[file_path.name] = str(file_path.resolve())
 
     return audio_files_dict
 
 
 # js交互py的api
-class pyApi:
+class pyWindowApi:
 
     def window_resizeTo(self, newWidth, newHeight):
         window.resize(newWidth, newHeight)
@@ -128,45 +126,51 @@ class pyApi:
         window.destroy()
 
 
-# 窗口状态更新
-def on_maximized():
-    window.maximized = True
-    window.minimized = False
+class windowEvents:
+    def __init__(self, window) -> None:
+        # 监听窗口控件
+        self.window = window
+        self.window.events.closed += self.on_closed
+        self.window.events.maximized += self.on_maximized
+        self.window.events.minimized += self.on_minimized
+        self.window.events.resized += self.on_resized
+        self.window.events.restored += self.on_restored
+        self.window.events.moved += self.on_moved
 
+    # 窗口状态更新
+    def on_maximized(self):
+        self.window.maximized = True
+        self.window.minimized = False
 
-def on_minimized():
-    window.maximized = False
-    window.minimized = True
+    def on_minimized(self):
+        self.window.maximized = False
+        self.window.minimized = True
 
+    def on_resized(self, width, height):
+        # print(f'窗口已调整大小，新尺寸为 {width} x {height}')
+        pass
 
-def on_resized(width, height):
-    # print(f'窗口已调整大小，新尺寸为 {width} x {height}')
-    pass
+    def on_moved(self, x, y):
+        pass
 
+    def on_restored(self):
+        # 窗口从最大化或最小化状态还原时触发。
+        if self.window.maximized and not self.window.minimized:
+            # 最大化还原到窗口
+            self.window.maximized = False
+            self.window.minimized = False
+        elif not self.window.maximized and self.window.minimized:
+            # 最小化还原到窗口
+            self.window.maximized = False
+            self.window.minimized = False
+        elif window.maximized and window.minimized:
+            # 最小化还原到最大化
+            self.window.maximized = True
+            self.window.minimized = False
 
-def on_moved(x, y):
-    pass
-
-
-def on_restored():
-    # 窗口从最大化或最小化状态还原时触发。
-    if window.maximized and not window.minimized:
-        # 最大化还原到窗口
-        window.maximized = False
-        window.minimized = False
-    elif not window.maximized and window.minimized:
-        # 最小化还原到窗口
-        window.maximized = False
-        window.minimized = False
-    elif window.maximized and window.minimized:
-        # 最小化还原到最大化
-        window.maximized = True
-        window.minimized = False
-
-
-def on_closed():
-    # print('窗口已关闭')
-    pass
+    def on_closed(self):
+        # print('窗口已关闭')
+        pass
 
 
 # 窗口控制器
@@ -181,41 +185,70 @@ window.addEventListener('DOMContentLoaded', function () {
     document.body.appendChild(JSElement);
 });
 
-
 """
     )
 
 
-api = pyApi()
-window = webview.create_window(
-    "html-music-player",
-    app,
-    width=width,
-    height=height,
-    min_size=(min_width, min_height),
-    resizable=True,
-    frameless=True,
-    easy_drag=False,
-    js_api=api,
-)
-
-# 监听窗口控件
-window.events.closed += on_closed
-window.events.maximized += on_maximized
-window.events.minimized += on_minimized
-window.events.resized += on_resized
-window.events.restored += on_restored
-window.events.moved += on_moved
-
-
-def main():
-    # 在前端的页面中绘制自定义按钮，并绑定事件
+def window_main():
+    global window
+    music_directory.append(Path.home() / "Music")
+    api = pyWindowApi()
+    window = webview.create_window(
+        "html-music-player",
+        app,
+        width=width,
+        height=height,
+        min_size=(min_width, min_height),
+        resizable=True,
+        frameless=True,
+        easy_drag=False,
+        js_api=api,
+    )
+    events = windowEvents(window)
     webview.start(
         evaluate_js,
         window,
-        debug=True,
+        # debug=True,
     )
     sys.exit()
+
+
+def android_main():
+    from jnius import autoclass
+
+    # 获取Android的Environment类
+    Environment = autoclass("android.os.Environment")
+    # 获取音乐文件夹的路径
+    music_dir = Environment.getExternalStoragePublicDirectory(
+        Environment.DIRECTORY_MUSIC
+    ).toString()
+    global music_directory
+    music_directory.append(music_dir)
+    window = webview.create_window(
+        "html-music-player",
+        app,
+    )
+    webview.start(
+        window,
+        # debug=True,
+    )
+    sys.exit()
+
+
+def main():
+    if sys.platform.startswith("win"):
+        # Windows系统
+        window_main()
+    elif sys.platform.startswith("linux"):
+        # Linux系统
+        window_main()
+    elif sys.platform.startswith("darwin"):
+        # macOS系统
+        window_main()
+    elif "android" in sys.platform:
+        # Android系统
+        android_main()
+    # 在前端的页面中绘制自定义按钮，并绑定事件
 
 
 if __name__ == "__main__":
